7206700964e7add624edb76f35a04bae7435e257
Jose Alanya <jose.alanya@gmail.com>
2017-03-31 16:18:57 -0500

    FEAT: JavaScript - Scopes

--- js/index.js
+++ js/index.js
@@ -28,3 +28,41 @@ if (!none) {
 if (aNumber === "10") {
   console.log("10 is 10")
 }
+
+// function showMsg(msg) {
+// console.log("showMsg " + msg);
+// }
+
+function showMsg(msg, more) {
+  if(more) {
+    console.log("showMsg+ " + msg + more);
+  } else {
+    console.log("showMsg+ " + msg);
+  }
+}
+
+showMsg("some information")
+showMsg("some information", " and even more");
+
+var showIt = function(msg) {
+ console.log(msg);
+}
+
+console.dir(showIt);
+
+var showIt2 = function showIt3(msg) {
+ console.log(msg);
+}
+
+showIt("Some message");
+showIt2("Some message");
+console.dir(showIt2);
+
+function showItThen(msg, callback) {
+  showIt(msg);
+  callback();
+}
+
+showItThen("showItThen called", function() {
+ console.log("callback called!");
+});
